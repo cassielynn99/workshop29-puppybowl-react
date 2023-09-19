0c3dfe4f1bae5d97da90bb9443095461c616f2ea
Cassandra Rockwell <cassielynn99@icloud.com>
2023-09-19 11:02:56 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+//to view: http://localhost:5173/
+//API: https://fsa-puppy-bowl.herokuapp.com/api/
+
+function App() {
+  return <></>;
+}
+
+export default App;
